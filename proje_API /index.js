009cf5e2fa103ad6f5e1cb005a294fe4bda8d589
D3NIZ1921 <wris8789@outlook.com>
2023-10-08 14:28:51 +0300

    API project
    Node

---
+++ proje_API /index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import axios from "axios";
+
+const app= express();
+const port= 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req,res) => {
+
+try{
+
+const result= await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+const cocktail= result.data.drinks[0];
+const imageUrl = cocktail.strDrinkThumb;
+
+
+res.render("index.ejs", { 
+
+    cocktail,
+    imageUrl,
+
+});
+
+}catch(error){
+
+    console.log(error.message);
+    res.status(500).send("Error.");
+}
+
+
+
+
+});
+
+app.listen(port, () => {
+
+    console.log(`Server is running on port ${port}`);
+
+
+});
+
+
+
+
+
